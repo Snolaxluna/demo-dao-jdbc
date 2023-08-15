80dbb3bdbd1d9ae02f358e8da9a0608df3c688b2
Snolaxluna <lucassolanich1987@gmail.com>
2023-08-15 07:34:21 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entites.Department;
+import model.entites.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
-        System.out.println("teste1");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
+
     }
 }
